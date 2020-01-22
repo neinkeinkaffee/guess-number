4f976f311ffc931d23fcdd738fb3fe2f798e6e19
Gesa Stupperich <gesa.stupperich@gmail.com>
2020-01-22 12:59:29 +0100

    Reports two correct out of place

--- src/test/java/com/thoughtworks/guessnumber/RaterTest.java
+++ src/test/java/com/thoughtworks/guessnumber/RaterTest.java
@@ -17,4 +17,16 @@ public class RaterTest {
         assertEquals(rating.getInPlaceMatches(), 1);
         assertEquals(rating.getOutOfPlaceMatches(), 0);
     }
+
+    @Test
+    public void should_report_two_correct_out_of_place() {
+        Solution solution = new Solution(1, 2, 3, 4);
+        Solution guess = new Solution(2, 4, 7, 8);
+        Rater rater = new Rater();
+
+        Rating rating = rater.rateGuess(guess, solution);
+
+        assertEquals(rating.getInPlaceMatches(), 0);
+        assertEquals(rating.getOutOfPlaceMatches(), 2);
+    }
 }
